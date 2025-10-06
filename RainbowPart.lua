8f598ed07559991bb60282ea8e299e58f4560a37
Renato Filho <renatofilho3344@gmail.com>
2025-10-06 12:14:27 -0300

    Criar RainbowPart.lua

---
+++ RainbowPart.lua
@@ -0,0 +1,21 @@
+local Part = game.Workspace:Wait for child("Part")
+local duration = 0.4
+
+while true do
+  Part.BrickColor = BrickColor.new("Really red")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Bright orange")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Bright yellow")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Lime green")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Pastel blue")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Bright Blue")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Bright violet")
+  wait(duration)
+  Part.BrickColor = BrickColor.new("Hot pink")
+  wait(duration)
+end
