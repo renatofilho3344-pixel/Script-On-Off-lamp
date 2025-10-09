928348d1a4bac1a526384a6340c69685cd996510
Renato Filho <renatofilho3344@gmail.com>
2025-10-09 14:25:14 -0300

    Atualizar o CleanTrash.lua

--- CleanTrash.lua
+++ CleanTrash.lua
@@ -1,10 +1,11 @@
 local Trash = script.Parent
-local prompt = script.Parent:Waitforchild("ProximityPrompt")
+local prompt = script.Parent:WaitForchild("ProximityPrompt")
 
 prompt.Triggered:Connect(function(player)
    prompt.Enabled = false
    for i = 1,10 do
       Trash.Transparency = Trash.Transparency + 0.1
+      wait(0.001)
    end
    wait(20) -- respawn time
    prompt.Enabled = true
